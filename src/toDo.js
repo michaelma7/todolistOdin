e9b160c020c11a799e50b41eff41d2d44c978029
michaelma7 <michaelma16688@gmail.com>
2024-02-28 13:34:51 -0500

    project init

---
+++ src/toDo.js
@@ -0,0 +1,18 @@
+// Properties that todo objects will have:
+// title
+// description 
+// due date
+// priority
+// recurring
+// how long(optional)
+// milestones within todo (mini checklist)
+
+// all of these can be changed
+
+// Properties of projects: 
+// can have multiple todos
+// each user will have a default project 'To-do'
+// users can have multiple projects (create, update, destroy)
+
+// calendar view:
+// can see when todos are due for each project (color coded or otherwise)
